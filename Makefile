dc833bf099f92cc38c7bf2697b5d5f3bb92e73bb
Robert W. Baumgartner <rwb@0x19e.net>
2019-03-03 18:52:59 -0600

    Refactor to build multiple variations for each certificate type.
    Signed-off-by: Robert W. Baumgartner <rwb@0x19e.net>

--- Makefile
+++ Makefile
@@ -11,16 +11,33 @@ ifeq ($(UNAME_O),Cygwin)
 endif
 
 OBJECTS = x509lint.o checks.o messages.o asn1_time.o
+OBJECTS_ROOT = x509lint-root.o checks.o messages.o asn1_time.o
+OBJECTS_INT = x509lint-int.o checks.o messages.o asn1_time.o
+OBJECTS_SUB = x509lint-sub.o checks.o messages.o asn1_time.o
+
+all: ${x509lint-root} x509lint-root ${x509lint-int} x509lint-int ${x509lint-sub} x509lint-sub
 
 x509lint: $(OBJECTS)
 	$(LD) $(LDFLAGS) -o $@ $(OBJECTS) $(LIBS)
+
+x509lint-root: $(OBJECTS_ROOT)
+	$(LD) $(LDFLAGS) -o $@ $(OBJECTS_ROOT) $(LIBS)
+
+x509lint-int: $(OBJECTS_INT)
+	$(LD) $(LDFLAGS) -o $@ $(OBJECTS_INT) $(LIBS)
+
+x509lint-sub: $(OBJECTS_SUB)
+	$(LD) $(LDFLAGS) -o $@ $(OBJECTS_SUB) $(LIBS)
 
 %.o: %.c
 	$(CC) $(CFLAGS) $< -c -o $@
 
 clean:
-	$(RM) -f x509lint *.o
+	$(RM) -f x509lint x509lint-sub x509lint-root x509lint-int *.o
 
 checks.o: checks.c checks.h
 x509lint.o: x509lint.c checks.h messages.h
+x509lint-root.o: x509lint-root.c checks.h messages.h
+x509lint-int.o: x509lint-int.c checks.h messages.h
+x509lint-sub.o: x509lint-sub.c checks.h messages.h
 messages.o: messages.c checks.h
---
+++ x509lint-sub.c
@@ -0,0 +1,99 @@
+/*
+ * Copyright (c) 2016 Kurt Roeckx <kurt@roeckx.be>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "checks.h"
+#include "messages.h"
+
+static int LoadCert(const char *filename, unsigned char **buffer, size_t *buflen)
+{
+	long size;
+	FILE *f;
+
+	f = fopen(filename, "rb");
+	if (f == NULL)
+	{
+		return -1;
+	}
+	if (fseek(f, 0, SEEK_END) != 0)
+	{
+		return -1;
+	}
+	size = ftell(f);
+	if (size == -1)
+	{
+		return -1;
+	}
+	*buffer = malloc(size);
+	if (fseek(f, 0, SEEK_SET) != 0)
+	{
+		free(*buffer);
+		*buffer = NULL;
+		return -1;
+	}
+	if (fread(*buffer, 1, size, f) != size)
+	{
+		free(*buffer);
+		*buffer = NULL;
+		return -1;
+	}
+	fclose(f);
+
+	*buflen = size;
+
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	unsigned char *buffer;
+	size_t buflen;
+
+	if (argc != 2)
+	{
+		printf("Usage: x509lint file\n");
+		exit(1);
+	}
+
+	if (LoadCert(argv[1], &buffer, &buflen) != 0)
+	{
+		fprintf(stderr, "Unable to read certificate\n");
+		exit(1);
+	}
+
+	check_init();
+	
+	check(buffer, buflen, PEM, SubscriberCertificate);
+
+	char *m = get_messages();
+	printf("%s", m);
+	free(m);
+
+	free(buffer);
+
+	check_finish();
+
+	return 0;
+}
+
